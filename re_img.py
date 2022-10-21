d100c69d92ccfccd6f73f43174fc0ef86885c8a7
Louis Tsang <kawingt2@gmail.com>
2022-10-21 22:41:34 +0800

    refactor to make code testable

--- re_img.py
+++ re_img.py
@@ -9,27 +9,30 @@ if len(sys.argv) < 2:
 if len(sys.argv) > 2:
     sys.exit("Too many command line arguments. Expecting 2 (including file name)")
 
-ACCEPTABLE_FILE_TYPE = [".jpg", ".jpeg", ".png"]
-
 def main():
     # check file type
     ext = os.path.splitext(sys.argv[1])[1]
- 
+
+    check_extension(ext)
+    
+    generate_img(sys.argv[1], ext)
+
+
+
+def check_extension(ext):
+    ACCEPTABLE_FILE_TYPE = [".jpg", ".jpeg", ".png"]
     if ext not in ACCEPTABLE_FILE_TYPE:
         sys.exit("Invalid file type. Only 'jpg', 'jpeg', and 'png' are acceptable")
-    
-    generate_img(ext)
 
-
-def generate_img(ext):
+def generate_img(file, ext):
     try:
-        test_file = open(sys.argv[1])
+        test_file = open(file)
         test_file.close()
     except FileNotFoundError:
         sys.exit("File not found. Please check your file")
     else:
         shirt = Image.open('./asset/shirt.png')
-        original = Image.open(sys.argv[1])
+        original = Image.open(file)
 
         size = shirt.size
 
@@ -38,6 +41,8 @@ def generate_img(ext):
         output.paste(shirt, box=None, mask=shirt)
 
         output.save(f'out_{randint(0, 100)}{ext}')
+
+        return True
 
 
 
